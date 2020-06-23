577fa2523832252d715d4e3789d7cfc7672bdcfd
Magnus <ByEldritchArt@gmail.com>
2020-06-23 08:40:42 -0500

    Fixed an issue with unfinished new mod conflicting with the old one

--- src/main/java/net/eldritch/client/addons/Prospector.java
+++ src/main/java/net/eldritch/client/addons/Prospector.java
@@ -8,8 +8,8 @@ public class Prospector {
 	private static HashMap<String, String> options;
 
 	public static void prospectorInit() {
-		String[] initOptions = { "Enabled(y/n):n", "Totems:0", "Elytra:0", "Test:0" };
-		EldritchClient.config.initializeOptions("AutoLog", initOptions);
-		options = EldritchClient.config.getOptionGroup("AutoLog");
+		String[] initOptions = {"Enabled(y/n):y"};
+		EldritchClient.config.initializeOptions("Prospector", initOptions);
+		options = EldritchClient.config.getOptionGroup("Prospector");
 	}
 }
